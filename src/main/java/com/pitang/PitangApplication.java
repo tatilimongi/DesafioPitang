e13e9feb0c3be857212bd8c7414d746ab2414a7b
Tatiana <tatianalimongi@gmail.com>
2024-05-01 18:08:29 -0300

    feat: springboot project creation

---
+++ src/main/java/com/pitang/PitangApplication.java
@@ -0,0 +1,13 @@
+package com.example.Pitang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PitangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Pitang/PitangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Pitang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
